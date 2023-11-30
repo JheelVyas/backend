c71a2973a4ed2968546ddf390f5b2b94b6b11136
JheelVyas <102655351+JheelVyas@users.noreply.github.com>
2023-11-30 20:11:44 +0530

    Add files via upload

---
+++ connection/conn.js
@@ -0,0 +1,3 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://jheel0696be21:DnzaZhGBGOLQqJaX@cluster0.tgemhd7.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>console.log("Connected"));
